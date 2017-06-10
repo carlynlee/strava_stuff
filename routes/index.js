cfe48927e7678f8063c58c4e5635e7a801c42d3a
Carlyn Lee <carlyn.lee@gmail.com>
2017-06-09 19:11:58 -0700

    first commit, get metars data from aviationweather.gov

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var parser = require('xml2json');
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -35,27 +37,46 @@ router.post('/adduser', function(req, res) {
 
     // Get our form values. These rely on the "name" attributes
     var userName = req.body.username;
-    var userEmail = req.body.useremail;
+    //var userEmail = req.body.useremail;
 
     // Set our collection
     var collection = db.get('usercollection');
 
-    // Submit to the DB
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            //res.location("userlist");
-            // And forward to success page
-            res.redirect("userlist");
-        }
+
+
+    http.get({
+        host: 'aviationweather.gov',
+        path: '/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString='+userName+'&hoursBeforeNow=1'
+    }, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            var contents_json=JSON.parse(parser.toJson(body));
+
+            // Submit to the DB
+            collection.insert({
+                "username" : userName,
+                "metars" : contents_json
+            }, function (err, doc) {
+                if (err) {
+                    // If it failed, return error
+                    res.send("There was a problem adding the information to the database.");
+                }
+                else {
+                    // If it worked, set the header so the address bar doesn't still say /adduser
+                    //res.location("userlist");
+                    // And forward to success page
+                    res.redirect("userlist");
+                }
+            });
+        });
     });
+
+
+
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var parser = require('xml2json');
+
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+
+
+
     res.send('respond with a resource');
 });
 
+
+
 module.exports = router;
+
+
+//https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KEMT,KAJO,KFUL, LGB&hoursBeforeNow=1
